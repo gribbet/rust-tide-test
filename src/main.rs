ab42a26dd61ef96c3bc8e3b6a47285ad2d9e5bf2
Graham <graham.gibbons@gmail.com>
2020-09-11 18:33:38 -0700

    Save

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate rust_tide_test;
 
-use rust_tide_test::{api::Api, database::DatabaseService, mock::MockService};
+use rust_tide_test::{api::Api, database::DatabaseService};
 use std::env;
 
 #[async_std::main]
@@ -8,7 +8,7 @@ async fn main() -> Result<(), std::io::Error> {
     let database_url =
         env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     tide::log::start();
-    let service = MockService::new(); //DatabaseService::new(database_url);
+    let service = DatabaseService::new(database_url);
     let api = Api::new(service);
     api.listen().await?;
     Ok(())
